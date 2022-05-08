61e75df4b162d15e6176833b622e06fda752775c
addr0x414b <addr0x414b@proton.me>
2022-05-08 12:38:53 -0600

    Set up SDL with CMAKE

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+#include <SDL2/SDL.h>
+
+int main() {
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_Window* window;
+	window = SDL_CreateWindow(
+			"Cuda Graphics",
+			SDL_WINDOWPOS_CENTERED,
+			SDL_WINDOWPOS_CENTERED,
+			640,
+			480,
+			0);
+
+	if (window == NULL) {
+		std::cout << "Failed to create window" << std::endl;
+		return 1;
+	}
+
+	SDL_Event event;
+	int running = 1;
+
+	SDL_Renderer* renderer;
+	renderer = SDL_CreateRenderer(window, -1, 0);
+
+	while (running) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				running = 0;
+			}
+		}
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderClear(renderer);
+		SDL_RenderPresent(renderer);
+	}
+
+}
