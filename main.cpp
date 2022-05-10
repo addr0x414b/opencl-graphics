cac1554358b1119e10111320c9ad36cd6de67943
addr0x414b <addr0x414b@proton.me>
2022-05-10 08:29:44 -0600

    Mock up cl code

--- main.cpp
+++ main.cpp
@@ -32,20 +32,21 @@ int main() {
 	cl::CommandQueue queue(context, device, 0, &error);
 	std::cout << "Queue Error Code: " << error << std::endl;
 
-	cl::EnqueueArgs args(queue, cl::NDRange(2));
+	float point[] = {
+		0.25f, 0.75f, 0.75f, 0.75f, 0.5f, 0.25f
+	};
+
+	cl::EnqueueArgs args(queue, cl::NDRange(sizeof(point) / sizeof(float)));
 
 	cl::Kernel kernel(program, "test", &error);
 	cl::KernelFunctor<> test(kernel);
 	std::cout << "Kernel Error Code: " << error << std::endl;
 
 
-	int point[] = {
-		320, 240
-	};
 
-	int out[2];
+	int out[6];
 	cl::Buffer inBuf(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
-			2*sizeof(point), point, &error);
+			6*sizeof(point), point, &error);
 	std::cout << "Error Code: " << error << std::endl;
 	error = kernel.setArg(0, sizeof(cl_mem), &inBuf);
 	std::cout << "Error Code: " << error << std::endl;
@@ -56,6 +57,31 @@ int main() {
 	test(args);
 
 	queue.enqueueReadBuffer(outBuf, CL_TRUE, 0, sizeof(out), out);
+
+	/*std::cout << out[0] << ", " << out[1] << std::endl;
+	std::cout << out[2] << ", " << out[3] << std::endl;
+	std::cout << out[4] << ", " << out[5] << std::endl;*/
+
+	cl::Kernel kernel2(program, "test2", &error);
+	cl::KernelFunctor<> test2(kernel2);
+	std::cout << "Kernel Error Code: " << error << std::endl;
+
+	cl::Buffer in2Buf(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+			6*sizeof(out), out, &error);
+	std::cout << "Error Code: " << error << std::endl;
+	error = kernel2.setArg(0, sizeof(cl_mem), &in2Buf);
+	std::cout << "Error Code: " << error << std::endl;
+
+	//uint32_t* textureBuffer = new uint32_t[640*480];
+	uint32_t textureBuffer[640*480];
+
+	cl::Buffer out2Buf(context, CL_MEM_WRITE_ONLY, sizeof(textureBuffer));
+	error = kernel2.setArg(1, out2Buf);
+	std::cout << "Error Code: " << error << std::endl;
+
+	test2(args);
+
+	queue.enqueueReadBuffer(out2Buf, CL_TRUE, 0, sizeof(textureBuffer), textureBuffer);
 
 	SDL_Init(SDL_INIT_VIDEO);
 
@@ -80,6 +106,19 @@ int main() {
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |
 			SDL_RENDERER_PRESENTVSYNC);
 
+	SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
+			SDL_TEXTUREACCESS_STREAMING, 640, 480);
+
+	/*textureBuffer[(out[1]*640) + out[0]] = 0xFF000000 | 0x00FF0000 | 0x0000FF00
+		| 0x000000FF;
+
+	textureBuffer[(out[3]*640) + out[2]] = 0xFF000000 | 0x00FF0000 | 0x0000FF00
+		| 0x000000FF;
+
+	textureBuffer[(out[5]*640) + out[4]] = 0xFF000000 | 0x00FF0000 | 0x0000FF00
+		| 0x000000FF;*/
+
+	SDL_UpdateTexture(texture, NULL, textureBuffer, 640*sizeof(uint32_t));
 
 	while (running) {
 		while (SDL_PollEvent(&event)) {
@@ -89,10 +128,13 @@ int main() {
 		}
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 		SDL_RenderClear(renderer);
-		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-		SDL_RenderDrawPoint(renderer, out[0], out[1]);
+		//SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+		//SDL_RenderDrawPoint(renderer, out[0], out[1]);
+		SDL_RenderCopy(renderer, texture, NULL, NULL);
 		SDL_RenderPresent(renderer);
 	}
 
+
 	SDL_Quit();
+	//delete[] textureBuffer;
 }
--- test.cl
+++ test.cl
@@ -1,4 +1,24 @@
-__kernel void test(__global int* points, __global int* output) {
+__kernel void test(__global float* points, __global int* output) {
 	int i = get_global_id(0);
-	output[i] = points[i];
+	if ((i+1) % 2 != 0) {
+		output[i] = points[i] * 640;
+	}
+	if ((i+1) % 2 == 0){
+		output[i] = points[i] * 480;
+	}
+}
+
+__kernel void test2(__global int* points, __global int* output) {
+	int i = get_global_id(0);
+	int x;
+	int y;
+	if ((i) % 2 == 0) {
+		x = points[i];
+		y = points[i+1];
+		printf("THIS: %d, %d\n", x, y);
+		output[(y*640) + x] = 0xFF000000 | 0x00FF0000 | 0x0000FF00
+			| 0x000000FF;
+	}
+	if ((i+1) % 2 == 0) {
+	}
 }
